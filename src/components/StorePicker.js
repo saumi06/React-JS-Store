3def75b9e6770e64cb7383bd6253637e5e99bf48
saumi06 <saumyajn1994@gmail.com>
2020-06-13 19:25:26 -0400

    Transition to store page

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component{
@@ -8,7 +9,10 @@ class StorePicker extends React.Component{
         event.preventDefault();
         console.log('you changed the url');
         console.log(this.storeInput.value);
+        const storeId = this.storeInput.value;
+        console.log(`Going to ${storeId}`);
         //transition from / to .store/storeid
+        this.props.history.push(`/store/${storeId}`);
 
     }
     
@@ -17,13 +21,20 @@ class StorePicker extends React.Component{
             <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
                 {/*Hello */}
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name"
-                defaultValue={getFunName()} ref={(input) => {this.storeInput = input}}/>
-                <button type="submit">Visit Store -{">"}</button>
+                <input 
+                type="text" 
+                required 
+                placeholder="Store Name"
+                defaultValue={getFunName()} 
+                ref={(input) => {this.storeInput = input}}/>
+                <button type="submit">Visit Store -&gt;</button>
             </form>
 
         )
     }
 }
 
+StorePicker.contextTypes = {
+    router: PropTypes.object.isRequired
+}
 export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { BrowserRouter as Router,Route} from 'react-router-dom';
 import {Switch} from 'react-router-dom'
 import './css/style.css';
-
+import PropTypes from 'prop-types';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
 import NotFound from './components/NotFound';
@@ -12,13 +12,12 @@ import NotFound from './components/NotFound';
 const Root = () => {
         return (
         <Router>
-                <div>
                 <Switch>
                         <Route exact path="/" component={StorePicker} />
                         <Route path="/store/:storeId" component={App} />
                         <Route component={NotFound} />
                </Switch>
-                </div>
+             
         </Router>
         )
 }
